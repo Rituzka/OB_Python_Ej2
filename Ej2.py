e97658abc3a01ee9b02d44ec6bdb56f64c7bcf56
Rita Casiello <rituzka@gmail.com>
2022-08-15 20:33:52 +0200

    first commit

---
+++ Ej2.py
@@ -0,0 +1,10 @@
+Python 3.10.6 (tags/v3.10.6:9c7b4bd, Aug  1 2022, 21:53:49) [MSC v.1932 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+edad = 37
+print(edad)
+37
+print(type(edad))
+<class 'int'>
+edad = 28
+print(edad)
+28
